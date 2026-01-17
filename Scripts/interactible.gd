aac6f1935c0bb45ca76208e5933d7017bc59d19d
JusPSLiu <juspsliu@yahoo.com>
2026-01-17 12:44:11 -0500

    refactored objects

--- Scripts/interactible.gd
+++ Scripts/interactible.gd
@@ -7,12 +7,6 @@ var player_in : bool = false
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _self_action():
 	print("no, u")
 
